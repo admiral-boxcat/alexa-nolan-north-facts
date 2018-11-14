ae40b9c83a2efcb5c910428612a6b8bd1693a5af
Robinson <username@robinsons-imac.lan>
2018-11-14 17:41:19 -0500

    🌌

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+const Alexa = require('alexa-sdk');
+
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
+//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
+const APP_ID = undefined;
+
+const SKILL_NAME = '';
+const GET_FACT_MESSAGE = "Here's your fact: ";
+const HELP_MESSAGE = '';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+//=========================================================================================================================================
+const data = [
+    '',
+    ''
+];
+
+//=========================================================================================================================================
+
+const handlers = {
+    'LaunchRequest': function () {
+      }
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    // Save state
+    'SessionEndedRequest': function() {
+        console.log('session ended!');
+        this.emit(':saveState', true);
+}
+};
+
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
